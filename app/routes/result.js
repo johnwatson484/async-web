d7036cd30838d53aa8957d69235daea0b176ec73
John Watson <johnwatson484@hotmail.com>
2021-07-10 21:02:19 +0100

    Change order

--- app/routes/result.js
+++ app/routes/result.js
@@ -5,13 +5,15 @@ module.exports = [{
   method: 'GET',
   path: '/result',
   handler: async (request, h) => {
+    let result = 'No response'
     const sessionQueueReceiver = new MessageReceiver(config.sessionQueueConfig)
     await sessionQueueReceiver.acceptSession(request.yar.id)
     const messages = await sessionQueueReceiver.receiveMessages(1, { maxWaitTimeInMs: 5000 })
-    await sessionQueueReceiver.closeConnection()
     if (messages.length) {
-      return h.view('result', { result: messages[0].body.content })
+      result = messages[0].body.content
+      await sessionQueueReceiver.completeMessage(messages[0])
     }
-    return h.view('result', { result: 'No response' })
+    await sessionQueueReceiver.closeConnection()
+    return h.view('result', { result })
   }
 }]
