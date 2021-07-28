55fdb164d69511199613b956c64be37652e94aaf
John Watson <johnwatson484@hotmail.com>
2021-07-28 21:47:50 +0100

    Updated route

--- app/routes/result.js
+++ app/routes/result.js
@@ -1,5 +1,6 @@
 const { MessageSender, MessageReceiver } = require('ffc-messaging')
 const config = require('../config')
+const { v4: uuidv4 } = require('uuid')
 
 module.exports = [{
   method: 'GET',
@@ -9,19 +10,20 @@ module.exports = [{
     if (!message) {
       return h.redirect('/')
     }
+    const messageId = uuidv4()
     const requestMessage = {
       body: { content: message },
       type: 'session test message',
-      subject: 'test',
       source: 'async web',
-      correlationId: request.yar.id
+      correlationId: request.yar.id,
+      messageId
     }
     const queueSender = new MessageSender(config.queueConfig)
     await queueSender.sendMessage(requestMessage)
     await queueSender.closeConnection()
     let result = 'No response'
     const sessionQueueReceiver = new MessageReceiver(config.sessionQueueConfig)
-    await sessionQueueReceiver.acceptSession(request.yar.id)
+    await sessionQueueReceiver.acceptSession(messageId)
     const messages = await sessionQueueReceiver.receiveMessages(1, { maxWaitTimeInMs: 5000 })
     if (messages.length) {
       result = messages[0].body.content
